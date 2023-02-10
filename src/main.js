d3b6a688f57e39a4fba7ae931fe3966ac2c09ac0
felipefll <felipeferreiralopeslima@gmail.com>
2023-02-10 20:45:26 -0300

    Main component creation

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import store from "./store/index.js";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+
 createApp(App).use(store).mount("#app");
